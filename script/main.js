a75a7e12936bf5db63c024ac5ed50b27666c93bf
danielius19 <danielius.stirbys19@my.northampton.ac.uk>
2020-08-06 19:41:05 +0100

    done

--- script/main.js
+++ script/main.js
@@ -1,8 +1,9 @@
+//JS for the dark theme button
 const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {document.body.classList.toggle('dark');
 });
-
+//JS for the github icon change
 function changeGit(){
     let git = document.getElementById('git');
     git.innerHTML = "&#xf09b;";
@@ -13,9 +14,9 @@ function changeGit(){
 }
 
 changeGit();
-
+//Sets the interval for the changes
 setInterval(changeGit, 2000);
-
+//JS for the linkedin icon change
 function changeLink(){
     let link = document.getElementById('linkedin');
     link.innerHTML = "&#xf0e1;";
@@ -26,9 +27,19 @@ function changeLink(){
 }
 
 changeLink();
-
+//Sets the interval for the changes
 setInterval(changeLink, 2000);
 
+//displays date after date and time button is clicked
 function displayDate() {
     document.getElementById('showDateTime').innerHTML = Date();
 }
+
+//Makes profile picture transition effect
+new hoverEffect({
+    parent: document.querySelector(".photo"),
+    intensity: 0.2,
+    image1: "./images/profile.jpg",
+    image2: "./images/profile2.jpg",
+    displacementImage: "./images/diss.png"
+});
