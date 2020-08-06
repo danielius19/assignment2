acb0757ee8094e8854c4203380eb8c13211aa6a9
danielius19 <danielius.stirbys19@my.northampton.ac.uk>
2020-08-06 15:08:21 +0100

    done

--- script/main.js
+++ script/main.js
@@ -2,3 +2,29 @@ const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {document.body.classList.toggle('dark');
 });
+
+function changeGit(){
+    let git = document.getElementById('git');
+    git.innerHTML = "&#xf09b;";
+
+    setTimeout(function(){
+        git.innerHTML = "&#xf092;"
+    }, 1000);
+}
+
+changeGit();
+
+setInterval(changeGit, 2000);
+
+function changeLink(){
+    let link = document.getElementById('linkedin');
+    link.innerHTML = "&#xf0e1;";
+
+    setTimeout(function(){
+        link.innerHTML = "&#xf08c;"
+    }, 1000);
+}
+
+changeLink();
+
+setInterval(changeLink, 2000);
